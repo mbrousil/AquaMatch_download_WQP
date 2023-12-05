b72d075c62f7f123bc570a9f66231dd32ff3a788
Matt Brousil <37380883+mbrousil@users.noreply.github.com>
2023-12-04 18:15:25 -0800

    Reshape download pipeline. Works

--- 2_download.R
+++ 2_download.R
@@ -305,21 +305,66 @@ p2_targets_list <- list(
   #   # cue = tar_cue("never")
   # ),
   
-  tar_target(p2_wqp_data_aoi,
-             bind_rows(
-               # p2_wqp_data_aoi_alk,
-               p2_wqp_data_aoi_chl,
-               p2_wqp_data_aoi_sdd, 
-               p2_wqp_data_aoi_tss,
-               p2_wqp_data_aoi_doc, 
-               # p2_wqp_data_aoi_temp,
-               # p2_wqp_data_aoi_phos, 
-               # p2_wqp_data_aoi_nitro,
-               # p2_wqp_data_aoi_depth, 
-               # p2_wqp_data_aoi_ssc,
-               # p2_wqp_data_aoi_poc
-             ),
-             format = "feather"),
+  # A named list of the datasets to facilitate exporting them to Google Drive
+  tar_target(p2_wqp_data_aoi_list,
+             list(
+               # "alkalinity" = p2_wqp_data_aoi_alk,
+               "chlorophyll" = p2_wqp_data_aoi_chl,
+               "doc" = p2_wqp_data_aoi_doc,
+               "tss" = p2_wqp_data_aoi_tss,
+               "sdd" = p2_wqp_data_aoi_sdd
+               # "temperature" = p2_wqp_data_aoi_temp,
+               # "phosphorus" = p2_wqp_data_aoi_phos, 
+               # "nitrogen" = p2_wqp_data_aoi_nitro,
+               # "ssc" = p2_wqp_data_aoi_ssc,
+               # "poc" = p2_wqp_data_aoi_poc
+             )),
+  
+  # Use {googledrive} to upload the final outputs of this pipeline, then return
+  # a file containing the links as text to be used outside of this pipeline
+  tar_file(p2_wqp_data_aoi_out_links,
+           {
+             # The result of this will be a data frame with one col for the
+             # parameter being exported and another col with a link to the
+             # exported data set in Google Drive
+             drive_links <- p2_wqp_data_aoi_list %>%
+               map2_df(.x = .,
+                       # Names of the sub-data sets above
+                       .y = names(.),
+                       .f = ~ {
+                         
+                         # We'll export each parameter's data set locally
+                         file_local_path <- paste0("2_download/out/p2_wqp_data_aoi_",
+                                                   .y,
+                                                   ".feather")
+                         write_feather(x = .x,
+                                       path = file_local_path)
+                         
+                         # Once locally exported, send to Google Drive
+                         out_file <- drive_put(media = file_local_path,
+                                               path = "~/aquamatch_download_wqp/")
+                         
+                         # Make the Google Drive link shareable: anyone can view
+                         out_file_share <- out_file %>%
+                           drive_share(role = "reader", type = "anyone")
+                         
+                         # Return labeled link to stack in a df and export
+                         tibble(parameter = .y,
+                                drive_link = drive_link(as_id(out_file_share$id)))
+                         
+                       })
+             
+             # Where the csv of Drive links is going
+             out_path <- "2_download/out/p2_wqp_data_aoi_out_links.csv"
+             
+             # Export the csv
+             write_csv(x = drive_links, file = out_path)
+             
+             # Return path to pipeline
+             out_path
+             
+           },
+           packages = c("tidyverse", "googledrive", "feather")),
   
   # Summarize the data downloaded from the WQP
   tar_target(
