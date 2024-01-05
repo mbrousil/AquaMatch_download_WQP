0a8312d07bc34ec995cc042421d608943389d447
Matt Brousil <37380883+mbrousil@users.noreply.github.com>
2024-01-05 09:37:00 -0800

    drive_auth()

--- run.R
+++ run.R
@@ -29,6 +29,8 @@ package_installer <- function(x) {
 # map function using base lapply
 lapply(required_pkgs, package_installer)
 
+googledrive::drive_auth()
+
 # load targets library
 library(targets)
 
