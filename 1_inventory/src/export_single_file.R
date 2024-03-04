7572fc9aecd88fdb9780f035072da138f3c5a20a
Matt Brousil <37380883+mbrousil@users.noreply.github.com>
2024-03-04 13:08:02 -0800

    Update export_single_file() def
    From targets_storage repo

--- 1_inventory/src/export_single_file.R
+++ 1_inventory/src/export_single_file.R
@@ -1,43 +1,71 @@
-# A function to export a single target (as a file) to Google Drive and return
-# the shareable Drive link as a filepath
-export_single_file <- function(target, folder_pattern){
+#' @title Export a single target to Google Drive
+#' 
+#' @description
+#' A function to export a single target (as a file) to Google Drive and return
+#' the shareable Drive link as a file path.
+#' 
+#' @param target The name of the target to be exported (as an object not a string).
+#' 
+#' @param drive_path A path to the folder on Google Drive where the file
+#' should be saved.
+#' 
+#' @param stable Logical value. If TRUE, also export the file to the "stable"
+#' subfolder in Google Drive. If FALSE, use the path as provided by the user.
+#' 
+#' @param google_email A string containing the gmail address to use for
+#' Google Drive authentication.
+#' 
+#' @returns 
+#' Returns a local path to a csv file containing a text link to the uploaded
+#' file in Google Drive.
+#' 
+export_single_file <- function(target, drive_path, stable, google_email){
   
-  require(googledrive)
+  # Authorize using the google email provided
+  drive_auth(google_email)
   
   # Get target name as a string
   target_string <- deparse(substitute(target))
   
-  # We'll export the dataset locally
-  file_local_path <- paste0(folder_pattern,
-                            target_string,
-                            ".rds")
+  # Create a temporary file exported locally, which can then be used to upload
+  # to Google Drive
+  file_local_path <- tempfile(fileext = ".rds")
   
   write_rds(x = target,
-            path = file_local_path)
+            file = file_local_path)
   
   # Once locally exported, send to Google Drive
   out_file <- drive_put(media = file_local_path,
-                        path = "~/aquamatch_download_wqp/")
+                        # The folder on Google Drive
+                        path = drive_path,
+                        # The filename on Google Drive
+                        name = paste0(target_string, ".rds"))
   
   # Make the Google Drive link shareable: anyone can view
-  out_file_share <- out_file %>%
-    drive_share(role = "reader", type = "anyone")
+  drive_share_anyone(out_file)
   
-  # Return labeled link to data in a df and export
-  link_table <- tibble(dataset = target_string,
-                       local_path = file_local_path,
-                       drive_link = drive_link(as_id(out_file_share$id)))
+  # If stable == TRUE then export a second, dated file to the stable/ subfolder
+  if(stable){
+    drive_path_stable <- paste0(drive_path, "stable/")
+    
+    # Once locally exported, send to Google Drive
+    out_file_stable <- drive_put(media = file_local_path,
+                                 # The folder on Google Drive
+                                 path = drive_path_stable,
+                                 # The filename on Google Drive
+                                 name = paste0(target_string,
+                                               "_",
+                                               gsub(pattern = "-",
+                                                    replacement = "",
+                                                    x = Sys.Date()),
+                                               ".rds"))
+    
+    # Make the Google Drive link shareable: anyone can view
+    drive_share(out_file_stable, role = "reader", type = "anyone")
+  }
   
-  # Where the csv with Drive link is going
-  out_path <- paste0(folder_pattern,
-                     target_string,
-                     "_out_link.csv")
-  
-  # Export the csv
-  write_csv(x = link_table, file = out_path)
-  
-  # Return path to pipeline
-  out_path
+  # Now remove the local file after upload is complete
+  file.remove(file_local_path)
   
 }
 
